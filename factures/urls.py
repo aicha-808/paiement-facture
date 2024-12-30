0ff3ce29e0e03f9ad16c6de4e61c409e20950fa8
aicha <aichatimbi74@gmail.com>
2024-12-30 17:42:35 +0100

    redirige la racine vers admin

--- factures/urls.py
+++ factures/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('admin', admin.site.urls), 
+    path('', lambda request: redirect('admin')),  # Redirige vers /admin
     # URLs pour l'authentification avec JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('', include('users.urls')),
-     path('api/', include('users.urls')), 
+    path('api/', include('users.urls')), 
 ]
